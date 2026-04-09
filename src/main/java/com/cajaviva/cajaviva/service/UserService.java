765683f3d7a802f11f7c611964db355389d575c1
Cristian Rojas Betancur <cristianrojas312682@correo.itm.edu.co>
2026-04-08 23:04:32 -0500

    feat(back): #11 user service layer

--- src/main/java/com/cajaviva/cajaviva/service/UserService.java
+++ src/main/java/com/cajaviva/cajaviva/service/UserService.java
@@ -3,16 +3,15 @@ package com.cajaviva.cajaviva.service;
 import com.cajaviva.cajaviva.entity.User;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 public interface UserService {
 
     List<User> findAll();
 
-    Optional<User> findById(UUID id);
+    User findById(UUID id);
 
-    User save(User user);
+    User create(User user);
 
     User update(UUID id, User user);
 
